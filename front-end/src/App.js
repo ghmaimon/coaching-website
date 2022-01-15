2c8ad1d47a1120b904e40405c3a8e5f239c97844
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-01-15 19:39:34 +0100

    imported header, not yet tested

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './css/App.css';
+// import Header from "./shared/header/Header";
+import MyButton from "./shared/MyButton";
+import TextButton from "./shared/TextButton";
+import Copyright from "./shared/Copyright";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyButton value="hello" />
+      <TextButton value="hello" />
+      <Copyright />
+      <h1>hello</h1>
     </div>
   );
 }
---
+++ front-end/src/shared/header/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import ContactUs from "./ContactUs";
+import AboutUs from "./AboutUs";
+import Logout from "./Logout";
+
+import Logo from "./Logo";
+import Cours from "./Cours";
+// import Search from "./Search";
+import Dashboard from "./Dashboard";
+import MyCourses from "./MyCourses";
+
+function Header() {
+  const [logedIn, setLogedIn] = useState(false);
+  useEffect(() => {
+    if (
+      localStorage.getItem("currentUser") !== undefined &&
+      localStorage.getItem("currentUser") !== null
+    )
+      setLogedIn(true);
+    console.log("header" + logedIn);
+  });
+  return (
+    <div>
+      {logedIn ? <Logout /> : <SignUp />}
+      {logedIn ? <Dashboard /> : <SignIn />}
+      {logedIn &&
+        (localStorage.getItem("isAdmin") === null ||
+          localStorage.getItem("isAdmin") === undefined) && <MyCourses />}
+      <ContactUs />
+      <AboutUs />
+      <Logo />
+      <Cours />
+      {/* {localStorage.getItem("isAdmin") !== "true" &&
+        localStorage.getItem("isStudent") !== "false" && <Search />} */}
+    </div>
+  );
+}
+export default Header;
