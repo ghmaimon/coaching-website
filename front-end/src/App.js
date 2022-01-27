4744bf79ddf2989efdef4d2b2f8ded2012967e0d
AADEL33 <adilabdelhalim3@gmail.com>
2022-01-27 19:44:43 +0100

    working on signup form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,22 @@ import Header from './shared/header/header';
 import Home from "./components/Home/Home";
 import Footer from "./shared/footer/Footer";
 import Profil from "./components/Dashboard/Client/profil";
+import SignUp from "./components/signup/SignUp"
+import SignIn from "./components/Signin/SignIn"
 
 function App() {
     console.log(Home);
     return (
 
-        <div className="body">
-            <Header/>
-            {/*<Home/>*/}
-            <Profil />
-
-            <Footer/>
-        </div>
+        <
+        div className = "body" >
+        <
+        Header / > { /*<Home/>*/ } { /* <Profil /> */ } <
+        SignUp / >
+        <
+        Footer / >
+        <
+        /div>
     );
 }
 
