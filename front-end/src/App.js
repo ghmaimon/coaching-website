394b201ab8e52009fea01bf15426aad81abbef07
Hajar <hajar.dami.14@gmail.com>
2022-02-20 14:38:08 +0100

    TWP-9 front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,23 +3,20 @@ import './css/App.css';
 import Header from './shared/header/header';
 import Home from "./components/Home/Home";
 import Footer from "./shared/footer/Footer";
-import Profil from "./components/Dashboard/Client/profil";
+import Profil from "./components/Dashboard/Admin/profil";
 import SignUp from "./components/signup/SignUp"
 import SignIn from "./components/Signin/SignIn"
+import AddOffer from './components/Offer/AddOffer/AddOffer';
 
 function App() {
     console.log(Home);
     return (
 
-        <
-        div className = "body" >
-        <
-        Header / > { /*<Home/>*/ } { /* <Profil /> */ } <
-        SignIn / >
-        <
-        Footer / >
-        <
-        /div>
+        <div className = "body" >
+        <Header /> { /*<Home/>*/ } { /* <Profil /> */ } 
+        <AddOffer/>
+        <Footer />
+        </div>
     );
 }
 
---
+++ front-end/src/components/Offer/Title.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "../../css/App.css"
+function Title() {
+  return ( <div>
+      <p style={{fontFamily:"Rubik Beastly",
+     
+    
+  lineHeight: "1,4",
+    fontSize: "25px",
+
+ fontWeight: "300",
+ marginTop:"10%"
+
+}}> <span style={{color:"rgba(15, 103, 8, 1)"}}> HELLO COACH ! </span>
+ 
+   Thank you for filling the following form to add a new offer  !</p>
+  
+ </div>
+  );
+}
+export default Title;
