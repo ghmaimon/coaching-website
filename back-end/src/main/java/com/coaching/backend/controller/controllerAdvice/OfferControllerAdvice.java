1af8507ef52af2671f6a14b66fc044d4c93afec1
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-02-24 15:35:58 +0100

    fixing issues within offerController

--- back-end/src/main/java/com/coaching/backend/controller/controllerAdvice/OfferControllerAdvice.java
+++ back-end/src/main/java/com/coaching/backend/controller/controllerAdvice/OfferControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.coaching.backend.controller.controllerAdvice;
 
 import com.coaching.backend.DTO.errorDTO.OfferError;
+import com.coaching.backend.exception.GeneralException;
 import com.coaching.backend.exception.OfferException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import java.time.Instant;
 import static com.coaching.backend.DTO.errorDTO.ExceptionJsonFormatter.convertExceptionToJson;
 
 public interface OfferControllerAdvice extends ControllerAdvice{
-    default <E extends OfferException> ResponseEntity<OfferError> handleOfferException(
+    default <E extends GeneralException> ResponseEntity<OfferError> handleOfferException(
             int statusCode, E exception) {
 
         LOG.debug("error response {}", exception.getMessage());
