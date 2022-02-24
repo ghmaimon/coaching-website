abf61dc00c354a64008e6ddec6815aafa390bb91
ghmaimon <kaixokoko123@gmail.com>
2022-02-24 15:48:39 +0100

    started verifying coach

--- back-end/src/main/java/com/coaching/backend/controller/controller/SuperUserController.java
+++ back-end/src/main/java/com/coaching/backend/controller/controller/SuperUserController.java
@@ -1,4 +1,20 @@
 package com.coaching.backend.controller.controller;
 
+import com.coaching.backend.service.SuperUserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+@RequestMapping("/api/account")
 public class SuperUserController {
+
+    SuperUserService superUserService;
+
+    public SuperUserController(SuperUserService superUserService) {
+        this.superUserService = superUserService;
+    }
+
+    p
 }
